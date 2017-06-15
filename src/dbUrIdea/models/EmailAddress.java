d299608edc22299e202b18741c29fe12f740e148
ytalo_202 <1310375@utp.edu.pe>
2017-06-15 13:43:44 -0500

    By Ytalo Classs EmailAdsreess amd Other class
    Signed-off-by: ytalo_202 <1310375@utp.edu.pe>

--- src/dbUrIdea/models/EmailAddress.java
+++ src/dbUrIdea/models/EmailAddress.java
@@ -4,21 +4,41 @@ package dbUrIdea.models;
  * Created by Magnus on 6/15/2017.
  */
 public class EmailAddress {
-    private String id;
+     private int id;
+     private String emailData;
 
-    public EmailAddress(String id) {
-        this.id = id;
-    }
 
     public EmailAddress() {
     }
 
-    public String getId() {
+    public EmailAddress(int id, String emailData) {
+        this.setId(id);
+        this.setEmailData(emailData);
+    }
+
+    public String getIdAsString() {
+        return String.valueOf(getId());
+    }
+
+    public String getEmailDataAsValue() {
+        return "'" + getEmailData() + "'";
+    }
+
+    public int getId() {
         return id;
     }
 
-    public EmailAddress setId(String id) {
+    public EmailAddress setId(int id) {
         this.id = id;
+        return this;
+    }
+
+    public String getEmailData() {
+        return emailData;
+    }
+
+    public EmailAddress setEmailData(String emailData) {
+        this.emailData = emailData;
         return this;
     }
 }
---
+++ src/dbUrIdea/models/EmailAdreessEntity.java
@@ -0,0 +1,65 @@
+package dbUrIdea.models;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Yoshinon on 15/06/2017.
+ */
+public class EmailAdreessEntity extends BaseEntity {
+
+    public EmailAdreessEntity(Connection connection) {
+
+        super(connection,"emails_address");
+    }
+
+    public EmailAdreessEntity() {
+        super();
+    }
+    List<EmailAdreessEntity> findAll() {
+        return findByCriteria("");
+    }
+    public EmailAddress findById(int id) {
+        String criteria = " id = " +
+                String.valueOf(id);
+        return findByCriteria(criteria).get(0);
+    }
+
+
+    public EmailAddress findByEmailData(String name) {
+        String criteria = " email_name = '" +
+                name + "'";
+        return findByCriteria(criteria).get(0);
+    }
+
+    public List<EmailAddress> findAllOrderedByEmail() {
+        String criteria = "true ORDER BY email_name";
+        return findByCriteria(criteria);
+    }
+
+    public List<EmailAddress> findByCriteria(String criteria) {
+        String sql = getDefaultQuery() +
+                criteria == "" ? "" : " WHERE " + criteria;
+        List<EmailAddress> emails_addreses = new ArrayList<>();
+        try {
+            ResultSet resultSet = getConnection()
+                    .createStatement()
+                    .executeQuery(sql);
+            if(resultSet == null) return null;
+            while(resultSet.next()) {
+                emails_addreses.add((new EmailAddress())
+                        .setId(resultSet.getInt("id"))
+                        .setEmailData(resultSet.getString("email_name")));
+            }
+            return emails_addreses;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
