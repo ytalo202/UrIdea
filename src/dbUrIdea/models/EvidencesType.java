d0beb521ba2f388f16ad50d0858f7c17e9376716
jhonatanluka <1411032@utp.edu.pe>
2017-06-15 11:27:58 -0500

    By jhon
    Signed-off-by: jhonatanluka <1411032@utp.edu.pe>

--- src/dbUrIdea/models/EvidencesType.java
+++ src/dbUrIdea/models/EvidencesType.java
@@ -4,4 +4,43 @@ package dbUrIdea.models;
  * Created by Yoshinon on 15/06/2017.
  */
 public class EvidencesType {
+    private int id;
+    private String name;
+    private String description;
+
+    public EvidencesType() {
+    }
+
+    public EvidencesType(int id, String name, String description) {
+        this.setId(id);
+        this.setName(name);
+        this.setDescription(description);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public EvidencesType setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public EvidencesType setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public EvidencesType setDescription(String description) {
+        this.description = description;
+        return this;
+    }
 }
