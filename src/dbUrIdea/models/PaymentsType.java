bb08d3cd95d601e96061c7118fea8cff3fd65266
jhonatanluka <1411032@utp.edu.pe>
2017-06-15 12:18:02 -0500

    By Ytalo Tuesta
    Signed-off-by: jhonatanluka <1411032@utp.edu.pe>

--- src/dbUrIdea/models/PaymentsType.java
+++ src/dbUrIdea/models/PaymentsType.java
@@ -4,4 +4,33 @@ package dbUrIdea.models;
  * Created by Usuario on 15/06/2017.
  */
 public class PaymentsType {
+    private int id;
+    private String name;
+
+
+    public PaymentsType() {
+    }
+
+    public PaymentsType(int id, String name) {
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public PaymentsType setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PaymentsType setName(String name) {
+        this.name = name;
+        return this;
+    }
 }
