8bfb3e28ff2a634e4edebae3bc9886283422c48a
jhonatanluka <1411032@utp.edu.pe>
2017-06-15 12:49:48 -0500

    By JHON TAMAYO
    Signed-off-by: jhonatanluka <1411032@utp.edu.pe>

--- src/dbUrIdea/models/Evidence.java
+++ src/dbUrIdea/models/Evidence.java
@@ -4,6 +4,43 @@ package dbUrIdea.models;
  * Created by UrIdea on 15/06/2017.
  */
 public class Evidence {
-    String id;
-    int IdEvidenceType;
+    private String id;
+    private EvidencesType evidencesType;
+    private Evaluation evaluation;
+
+    public Evidence() {
+    }
+
+    public Evidence(String id, EvidencesType evidencesType, Evaluation evaluation) {
+        this.setId(id);
+        this.setEvidencesType(evidencesType);
+        this.setEvaluation(evaluation);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Evidence setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public EvidencesType getEvidencesType() {
+        return evidencesType;
+    }
+
+    public Evidence setEvidencesType(EvidencesType evidencesType) {
+        this.evidencesType = evidencesType;
+        return this;
+    }
+
+    public Evaluation getEvaluation() {
+        return evaluation;
+    }
+
+    public Evidence setEvaluation(Evaluation evaluation) {
+        this.evaluation = evaluation;
+        return this;
+    }
 }
