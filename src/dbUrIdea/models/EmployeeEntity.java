d5b0dead78657588b116cc98de218b2787928e68
ytalo_202 <1310375@utp.edu.pe>
2017-06-16 10:27:24 -0500

    By Ytalo Classs EmployessEntity
    Signed-off-by: ytalo_202 <1310375@utp.edu.pe>

--- src/dbUrIdea/models/EmployeeEntity.java
+++ src/dbUrIdea/models/EmployeeEntity.java
@@ -1,7 +1,22 @@
 package dbUrIdea.models;
 
+import java.util.Date;
+
 /**
  * Created by Usuario on 15/06/2017.
  */
 public class EmployeeEntity {
+    String id;
+    Company company;
+    UserType userType;
+    EmailAddress emailAddress;
+    String password;
+    String name;
+    String firstLastName;
+    String secondLastName;
+    int phoneNumber;
+    int cellPhoneNumber;
+    String address;
+    String department;
+    Date birthdate;
 }
