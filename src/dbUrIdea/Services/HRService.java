2206e52f0bdb032ca5769d346b963b09d388e0e4
anthonyllanos3008 <1212942@utp.edu.pe>
2017-06-22 12:12:42 -0500

    By Gerson;implement HRService
    Signed-off-by: anthonyllanos3008 <1212942@utp.edu.pe>

--- src/dbUrIdea/Services/HRService.java
+++ src/dbUrIdea/Services/HRService.java
@@ -1,7 +1,53 @@
 package dbUrIdea.Services;
 
+import dbUrIdea.models.Company;
+import dbUrIdea.models.HRDataStore;
+
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * Created by Yoshinon on 21/06/2017.
  */
 public class HRService {
+
+    private Connection connection;
+    private HRDataStore dataStore;
+
+    public HRService(InitialContext ctx) {
+        try {
+            connection = ((DataSource) ctx.lookup("jdbc/MySQLDataSource"))
+                    .getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Connection getConnection() {
+        return connection;
+    }
+
+    private HRDataStore getDataStore() {
+        if(dataStore == null) {
+            dataStore = new HRDataStore(getConnection());
+        }
+        return dataStore;
+    }
+
+    public List<Company> findAllRegions() {
+        return getDataStore().findAllCompanies();
+    }
+
+
+
+
+
+
 }
