7c87018621040d9158d5099ea2be3c653e4bf0f2
ytalo20 <131037@utp.edu.pe>
2017-07-07 13:15:32 -0500

    by anonimo

---
+++ src/dbUrIdea/viewcontrollers/PaymentsTypeServlet.java
@@ -0,0 +1,22 @@
+package dbUrIdea.viewcontrollers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Yoshinon on 7/07/2017.
+ */
+@WebServlet(name = "PaymentsTypeServlet")
+public class PaymentsTypeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
